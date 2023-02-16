249ef58e18e89049bfde5a18427ea22a3e3ce1c2
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2023-02-16 21:33:15 +0200

    Added First Steps In Coding - Exercise

---
+++ Programming Basics C#/2.First Steps In Coding - Exercise/01. USD to BGN/01. USD to BGN/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _01._USD_to_BGN
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double money = double.Parse(Console.ReadLine()) * 1.79549;
+
+            Console.WriteLine(money);
+        }
+    }
+}
