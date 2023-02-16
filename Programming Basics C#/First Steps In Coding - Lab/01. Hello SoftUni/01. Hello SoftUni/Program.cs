f11c541fc22349737866a0dc531cc6deb57245e2
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2023-02-16 21:20:02 +0200

    fixed 01 exercises

---
+++ Programming Basics C#/First Steps In Coding - Lab/01. Hello SoftUni/01. Hello SoftUni/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._Hello_SoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
