a06feba2fd3327ef97ffa254b5ca83123e3a5476
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2023-02-19 09:56:52 +0200

    Added new exercise

---
+++ 2.Fundamentals C#/13.Lists - Exercise/03. House Party/03. House Party/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _03._House_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                List<string> input = Console.ReadLine().Split().ToList();
+
+                if (input[2] == "not")
+                {
+                    if (names.Contains(input[0]))
+                    {
+                        names.Remove(input[0]);
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine(input[0] + " is not in the list!");
+                        continue;
+                    }
+                }
+
+                if (names.Contains(input[0]))
+                {
+                    Console.WriteLine(input[0]+" is already in the list!");
+                    continue;
+                }
+
+                
+                names.Add(input[0]);
+
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ 2.Fundamentals C#/13.Lists - Exercise/04. List Operations/04. List Operations/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace _04._List_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            
+
+            while (true)
+            {
+                List<string> command = Console.ReadLine().Split().ToList();
+                List<int> shiftedList = new List<int>(input.Count);
+
+                if (command[0] == "End")
+                {
+                    Console.WriteLine(string.Join(" ", input));
+                    Environment.Exit(0);
+                }
+
+                if (command[0] == "Add")
+                {
+                    input.Add(int.Parse(command[1]));
+                }
+                else if (command[0] == "Insert")
+                {
+                    if (int.Parse(command[2]) > input.Count || int.Parse(command[2]) < 0)
+                    {
+                        Console.WriteLine($"Invalid index");
+                        continue;
+                    }
+                    input.Insert(int.Parse(command[2]), int.Parse(command[1]));
+                }
+                else if (command[0] == "Remove")
+                {
+
+                    if (int.Parse(command[1]) > input.Count || int.Parse(command[1])<0)
+                    {
+                        Console.WriteLine($"Invalid index");
+                        continue;
+                    }
+
+                    input.RemoveAt(int.Parse(command[1]));
+                }
+                else if (command[0] == "Shift" && command[1] == "left")
+                {
+                    for (int j = 0; j < int.Parse(command[2]); j++)
+                    {
+                        int lastIndex = input[0];
+
+                        for (int i = 0; i < input.Count-1; i++)
+                        {
+                            shiftedList.Add(input[i + 1]);
+                        }
+                        shiftedList.Add(lastIndex);
+
+                        for (int i = 0; i < input.Count; i++)
+                        {
+                            input[i] = shiftedList[i];
+                        }
+                        shiftedList.Clear();
+
+                    }
+                }
+                else if (command[0] == "Shift" && command[1] == "right")
+                {
+                    for (int j = 0; j < int.Parse(command[2]); j++)
+                    {
+                        int lastIndex = input[^1];
+                        shiftedList.Add(lastIndex);
+                        for (int i = 0; i < input.Count-1; i++)
+                        {
+                            shiftedList.Add(input[i]);
+                        }
+
+                        for (int i = 0; i < input.Count; i++)
+                        {
+                            input[i] = shiftedList[i];
+                        }
+                        shiftedList.Clear();
+                    }
+                }
+            }
+        }
+    }
+}
