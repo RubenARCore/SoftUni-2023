be5d9329fe3519c6de7c9d6d42a92a815a1f171f
Ruben Arutyunyan <yordanov.ruben@gmail.com>
2023-11-05 22:43:03 +0200

    Added Arrays - More Exercise

---
+++ 2.Fundamentals C#/9. Arrays - More Exercise/01. Encrypt, Sort and Print Array/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            if (n == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            int[] result = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+
+                string input = Console.ReadLine();
+
+                char[] charInput = input.ToCharArray();
+
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (charInput[j] == 'a' || charInput[j] == 'e'
+                                            || charInput[j] == 'i'
+                                            || charInput[j] == 'o'
+                                            || charInput[j] == 'u'
+                                            || charInput[j] == 'A'
+                                            || charInput[j] == 'E'
+                                            || charInput[j] == 'I'
+                                            || charInput[j] == 'O'
+                                            || charInput[j] == 'U')
+                    {
+                        sum += charInput[j] * input.Length;
+                    }
+                    else
+                    {
+                        sum += charInput[j] / input.Length;
+                    }
+                }
+                result[i] = sum;
+            }
+            Array.Sort(result);
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(result[j]);
+            }
+
+        }
+    }
+}
